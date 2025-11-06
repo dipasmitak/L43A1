8a8eec60b0d8e5282cc8318425df452051539cc9
dipasmitak <dipasmita.kashyap@gmail.com>
2025-11-06 19:18:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+print("🧮Sum of Whole Number🧮\n")
+
+n = int(input("Enter the last whole number: "))
+
+sum = 0
+for i in range(n + 1):
+    sum = sum + i
+
+print("\n The sum of whole numbers from 0 to", n, "is:", sum)
+
